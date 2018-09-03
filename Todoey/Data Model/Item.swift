4324701f36bf12fd9ec57f9cdd5ffca1d3771376
James Bankston <jpbankston@mac.com>
2018-09-03 14:33:46 -0700

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by James Bankston on 9/3/18.
+//  Copyright © 2018 James Bankston. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+}
